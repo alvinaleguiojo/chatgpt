8cf1af08a4d602380cacaa6ccaa33e7bcea9a605
alvinaleguiojo <alvin.aleguiojo@gmail.com>
2023-02-28 02:33:51 +0800

    type prompt

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -6,6 +6,7 @@ import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { db } from "../firebase";
 import Message from "./Message";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/solid";
 
 type Props = {
   chatId: string;
@@ -25,15 +26,21 @@ function Chat({ chatId }: Props) {
           chatId,
           "messages"
         ),
-        orderBy("createdAt","asc")
+        orderBy("createdAt", "asc")
       )
   );
 
   return (
-    <div className="flex-1 overflow-auto">
-     {messages?.docs.map((message) =>(
-      <Message key={message.id} message={message.data()}/>
-     ))}
+    <div className="flex-1 overflow-y-auto overflow-x-hidden">
+      {messages?.empty && (
+        <>
+          <p className="mt-10 text-center text-white">Type a prompt in below to get started!</p>
+          <ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 text-white animate-bounce" />
+        </>
+      )}
+      {messages?.docs.map((message) => (
+        <Message key={message.id} message={message.data()} />
+      ))}
     </div>
   );
 }
